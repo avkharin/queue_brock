05d2a5bac7a44c4919c3f6a707c5500894a2203f
Alexander Kh <av.kharin@gmail.com>
2024-12-12 14:42:50 +0500

    Implement method Enqueue(msg string) of Queue struct

--- main.go
+++ main.go
@@ -12,7 +12,16 @@ type Queue struct {
 }
 
 func (q *Queue) Enqueue(msg string) {
-
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	if len(q.waiters) > 0 {
+		waiter := q.waiters[0]
+		q.waiters = q.waiters[1:]
+		waiter <- msg
+		close(waiter)
+	} else {
+		q.messages = append(q.messages, msg)
+	}
 }
 
 func (q *Queue) Dequeue(timeout time.Duration) (string, bool) {
